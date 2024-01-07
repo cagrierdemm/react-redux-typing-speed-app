aec509e454f6e5b1d1787ab29269b8c5a026b2af
Çağrı Erdem <cagrierdem001@gmail.com>
2024-01-08 00:28:13 +0300

    responsive bug fix

--- src/App.js
+++ src/App.js
@@ -82,8 +82,8 @@ function App() {
 
   return (
     <div className='container'>
-      <div className='row align-items-center'>
-        <div className='col-md-8 row mx-auto text-center lightBox p-2 row d-flex justify-content-center align-items-center'>
+      <div className='row col-md-8 col-12 align-items-center mx-auto'>
+        <div className='row mx-auto text-center lightBox p-2 d-flex justify-content-center align-items-center'>
           <div className='col-3 col-md-2'>
             <select className='rounded-3' onChange={langChange}>
               <option value='TR'>Türkçe</option>
@@ -96,28 +96,28 @@ function App() {
           <div className='col-3 col-md-2'>
             <a href='https://github.com/cagrierdemm'><img src='./github.png' className='float-end lightLogo' alt='Github Logo' width={30} /></a>
             <a href='https://www.linkedin.com/in/cagrierdemm/'><img src='./linkedin.png' className='lightLogo' alt='Linkedin Logo' width={30} /></a>
-            </div>
+          </div>
         </div>
+      </div>
+
+      <div className='row col-md-8 col-12 align-items-center mx-auto lightBox p-4 mt-5 mb-2 '>
+        <div className='bg-white py-4 border-0 rounded-3 mt-3' style={{ maxHeight: "180px", overflowY: "hidden" }}>
+          {words.map((word, key) => (
+            <span key={key} ref={key === i ? setTargetWordRef : null} className={`p-1 pt-4 fs-4 mb-4`}><span className={`p-1 text-${wordStatus[key]} ${key === i && 'bg-secondary-subtle'}`}>{lang === 'TR' ? word.turkish : word.english}</span></span>
+          ))}
         </div>
-        <div className='row'>
-        <div className='col-md-8 mx-auto lightBox p-5 mt-5 mb-5'>
-          <div className='row'><div className='bg-white py-4 border-0 rounded-3' style={{ maxHeight: "180px", overflowY: "hidden" }}>
-            {words.map((word, key) => (
-              <span key={key} ref={key === i ? setTargetWordRef : null} className={`p-1 pt-4 fs-4 mb-4`}><span className={`p-1 text-${wordStatus[key]} ${key === i && 'bg-secondary-subtle'}`}>{lang === 'TR' ? word.turkish : word.english}</span></span>
-            ))}
-          </div></div>
-          <div className='text-center'>
-            <input className='mt-5 py-2 px-3 rounded-2 border-0' type='text' value={inputValue} onChange={handleChange}></input>
-            <span className='mx-2 bg-dark rounded-2 border-0  text-white p-2 mrg'>{time}</span>
-            <button className='me-2 p-2 rounded-2 border-0 text-white hover mrg' onClick={handleReset}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
-                <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
-              </svg>
-            </button></div>
-        </div>
-        </div>
-     
+        <div className='text-center'>
+          <input className='mt-5 py-2 px-3 rounded-2 border-0' type='text' value={inputValue} onChange={handleChange}></input>
+          <span className='mx-2 bg-dark rounded-2 border-0  text-white p-2 mrg'>{time}</span>
+          <button className='me-2 p-2 rounded-2 border-0 text-white hover mrg' onClick={handleReset}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
+              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
+            </svg>
+          </button></div>
+      </div>
+
+
       {modalVisible && (
         <div className="modal" id='exampleModal' tabindex="-1" style={{ display: 'block', backgroundColor: 'rgba(0, 0, 0, 0.5)' }} data-bs-target="#exampleModal">
           <div className="modal-dialog modal-dialog-centered text-center">
