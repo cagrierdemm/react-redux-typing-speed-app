833de65b8e511f8772cb881c00af819a15bf1fd2
Çağrı Erdem <cagrierdem001@gmail.com>
2024-01-07 06:08:08 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -106,8 +106,8 @@ function App() {
           </div></div>
           <div className='text-center'>
             <input className='mt-5 py-2 px-3 rounded-2 border-0' type='text' value={inputValue} onChange={handleChange}></input>
-            <span className='mx-2 bg-dark rounded-2 border-0  text-white p-2'>{time}</span>
-            <button className='me-2 p-2 rounded-2 border-0 text-white hover' onClick={handleReset}>
+            <span className='mx-2 bg-dark rounded-2 border-0  text-white p-2 mrg'>{time}</span>
+            <button className='me-2 p-2 rounded-2 border-0 text-white hover mrg' onClick={handleReset}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-clockwise" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z" />
                 <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
